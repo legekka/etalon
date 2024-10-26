95a9055c850fc8995ba278be2b63ff3dd47c5c11
legekka <legekka@gmail.com>
2024-10-26 23:14:17 +0200

    Fix loss function in BalancedTrainer class

--- modules/custom_trainers.py
+++ modules/custom_trainers.py
@@ -5,7 +5,7 @@ class BalancedTrainer(Trainer):
         labels = inputs.get("labels")
         outputs = model(**inputs)
         logits = outputs.logits
-        loss = self.loss_fn(logits, labels)
+        loss = self.custom.loss_fn(logits, labels)
         return (loss, outputs) if return_outputs else loss
 
     def setup_custom_trainer_class_weights(self, loss_fn):
